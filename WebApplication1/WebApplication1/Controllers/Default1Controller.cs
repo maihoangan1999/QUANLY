8b324dfcf2f86e7840228e06a11b1a06c2c4d31e
maihoangan1999 <maihoangan1999@gmail.com>
2019-04-18 00:38:15 +0700

    HoangAn-Test

---
+++ WebApplication1/WebApplication1/Controllers/Default1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+
+namespace WebApplication1.Controllers
+{
+    public class Default1Controller : Controller
+    {
+        private QUANLYMATHANGEntities db = new QUANLYMATHANGEntities();
+
+        // GET: /Default1/
+        public ActionResult Index()
+        {
+            return View(db.SANPHAMs.ToList());
+        }
+
+        // GET: /Default1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sanpham = db.SANPHAMs.Find(id);
+            if (sanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanpham);
+        }
+
+        // GET: /Default1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Default1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,TenSP,GiaSP,MaSP,TinhTrang")] SANPHAM sanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SANPHAMs.Add(sanpham);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sanpham);
+        }
+
+        // GET: /Default1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sanpham = db.SANPHAMs.Find(id);
+            if (sanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanpham);
+        }
+
+        // POST: /Default1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,TenSP,GiaSP,MaSP,TinhTrang")] SANPHAM sanpham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sanpham).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sanpham);
+        }
+
+        // GET: /Default1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SANPHAM sanpham = db.SANPHAMs.Find(id);
+            if (sanpham == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sanpham);
+        }
+
+        // POST: /Default1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SANPHAM sanpham = db.SANPHAMs.Find(id);
+            db.SANPHAMs.Remove(sanpham);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
